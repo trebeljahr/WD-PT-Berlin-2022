3b65d753ecba2529a8d34468a6c66bea5b43b92d
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-10-29 12:56:42 +0200

    Add comment on linked lists vs. arrays

--- week16/day3/live/props-again/src/components/Comments/Comments.jsx
+++ week16/day3/live/props-again/src/components/Comments/Comments.jsx
@@ -14,3 +14,14 @@ export const Comments = ({ comments, toggleLikeOnComment }) => {
     </div>
   );
 };
+
+// node, ptr => node
+
+// n1 => n2 => n3 => n1
+// n1 = { next: n2 }
+// n2 = { next: n3 }
+
+// n1.next.next.next
+
+// [0, 1, 2, 3]
+// 011010101 010010101 0101001011 0101010101
