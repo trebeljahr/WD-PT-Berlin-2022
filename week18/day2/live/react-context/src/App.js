e7c6ab3398273af702fa697b1401efdfd025a283
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-11-10 18:53:18 +0100

    The problem prob drilling

--- week18/day2/live/react-context/src/App.js
+++ week18/day2/live/react-context/src/App.js
@@ -1,7 +1,35 @@
 import "./App.css";
+import { useState } from "react";
+
+function Text({ isDarkMode }) {
+  return (
+    <p style={{ color: isDarkMode ? "white" : "black" }}>Lorem Ipsum...</p>
+  );
+}
+
+function Header({ isDarkMode }) {
+  return <h1 style={{ color: isDarkMode ? "white" : "black" }}>Hello World</h1>;
+}
+
+function LoremIpsumBody({ isDarkMode }) {
+  return (
+    <>
+      <Header isDarkMode={isDarkMode} />
+      <Text isDarkMode={isDarkMode} />
+    </>
+  );
+}
 
 function App() {
-  return <h1>Hello World</h1>;
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleDarkMode = () => setIsDarkMode((old) => !old);
+
+  return (
+    <div style={{ backgroundColor: isDarkMode ? "rgb(50, 30, 100)" : "white" }}>
+      <button onClick={toggleDarkMode}>Change Theme!</button>
+      <LoremIpsumBody isDarkMode={isDarkMode} />
+    </div>
+  );
 }
 
 export default App;
